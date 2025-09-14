477205d4b11321f623f895ad6710af284e582dce
Saad <saadaulakh04@gmail.com>
2025-09-14 17:27:50 -0400

    Made Electron work

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // Load React dev server (when in dev)
+  win.loadURL('http://localhost:3000'); // Or whatever port your React app runs on
+
+  // Uncomment to open DevTools
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
